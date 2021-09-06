e93969b2476b37bf80c258be9b5318b386a06ba9
ANIX <88741448+JonyNoise@users.noreply.github.com>
2021-09-06 08:43:32 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,6 +4,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/octavi_mojito.mk
 
     COMMON_LUNCH_CHOICES := \
-    corvus_mojito-user \
-    corvus_mojito-userdebug \
-    corvus_mojito-eng
+    octavi_mojito-user \
+    octavi_mojito-userdebug \
+    octavi_mojito-eng
